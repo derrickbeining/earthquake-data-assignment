25917e484ddef5edd0c7b414c71fe13e36e449c9
Derrick Beining <derrick.beining@gmail.com>
2017-11-09 03:18:30 -0500

    more tidying up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {AppContainer} from "react-hot-loader";
-import App from "./components/App";
+import {AppContainer} from 'react-hot-loader';
+import App from './components/App';
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root');
 
 const renderComponent = (Component) => {
     ReactDOM.render(
@@ -18,7 +18,7 @@ renderComponent(App);
 
 // Hot Module Replacement API
 if (module.hot) {
-    module.hot.accept("./components/App", () => {
+    module.hot.accept('./components/App', () => {
         renderComponent(App);
     });
 }
