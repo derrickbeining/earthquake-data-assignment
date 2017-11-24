d76f75fd74366092fc59cf675ba5b19b022b0bed
Derrick Beining <derrick.beining@gmail.com>
2017-11-24 02:58:15 -0500

    corrected getDay() to getDate()

--- src/utils/convertDate.js
+++ src/utils/convertDate.js
@@ -2,7 +2,7 @@ const convertDate = dateString => {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const dt = new Date(dateString);
 
-  return `${months[dt.getMonth()]} ${dt.getDay()}, ${dt.getFullYear()} @ ${dt.getHours()}:${dt.getMinutes()}`
+  return `${months[dt.getMonth()]} ${dt.getDate()}, ${dt.getFullYear()} @ ${dt.getHours()}:${dt.getMinutes()}`
 }
 
 export default convertDate;
